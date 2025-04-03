89fcc2d0d8a7e33baf597eb58566070d552cf70e
Ishant Bhatt <ishantbhatt1654@gmail.com>
2025-04-03 18:14:18 +0530

    changes:- vercel error fix

--- src/app/visa-assistance/page.js
+++ src/app/visa-assistance/page.js
@@ -1,6 +1,6 @@
+import { Timeline } from "@/components/ui/Timeline";
 import Image from "next/image";
 import React from "react";
-import { Timeline } from "@/components/ui/timeline";
 
 export default function Page() {
   const data = [
